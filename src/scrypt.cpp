cb82fe632f6c1980548089b923c00e50b8875761
Cory Fields <theuni-nospam-@xbmc.org>
2013-09-05 17:25:16 -0400

    sse: break apart sse2/non-sse2 into separate objects
    This way they can be chosen at runtime (still todo)

--- src/scrypt.cpp
+++ src/scrypt.cpp
@@ -49,23 +49,6 @@ static inline void be32enc(void *pp, uint32_t x)
 	p[0] = (x >> 24) & 0xff;
 }
 
-static inline uint32_t le32dec(const void *pp)
-{
-	const uint8_t *p = (uint8_t const *)pp;
-	return ((uint32_t)(p[0]) + ((uint32_t)(p[1]) << 8) +
-	    ((uint32_t)(p[2]) << 16) + ((uint32_t)(p[3]) << 24));
-}
-
-static inline void le32enc(void *pp, uint32_t x)
-{
-	uint8_t *p = (uint8_t *)pp;
-	p[0] = x & 0xff;
-	p[1] = (x >> 8) & 0xff;
-	p[2] = (x >> 16) & 0xff;
-	p[3] = (x >> 24) & 0xff;
-}
-
-
 typedef struct HMAC_SHA256Context {
 	SHA256_CTX ictx;
 	SHA256_CTX octx;
@@ -139,7 +122,7 @@ HMAC_SHA256_Final(unsigned char digest[32], HMAC_SHA256_CTX *ctx)
  * Compute PBKDF2(passwd, salt, c, dkLen) using HMAC-SHA256 as the PRF, and
  * write the output to buf.  The value dkLen must be at most 32 * (2^32 - 1).
  */
-static void
+void
 PBKDF2_SHA256(const uint8_t *passwd, size_t passwdlen, const uint8_t *salt,
     size_t saltlen, uint64_t c, uint8_t *buf, size_t dkLen)
 {
@@ -190,110 +173,6 @@ PBKDF2_SHA256(const uint8_t *passwd, size_t passwdlen, const uint8_t *salt,
 	/* Clean PShctx, since we never called _Final on it. */
 	memset(&PShctx, 0, sizeof(HMAC_SHA256_CTX));
 }
-
-
-#ifdef __SSE2__
-
-#include <emmintrin.h>
-
-static inline void xor_salsa8(__m128i B[4], const __m128i Bx[4])
-{
-	__m128i X0, X1, X2, X3;
-	__m128i T;
-	int i;
-
-	X0 = B[0] = _mm_xor_si128(B[0], Bx[0]);
-	X1 = B[1] = _mm_xor_si128(B[1], Bx[1]);
-	X2 = B[2] = _mm_xor_si128(B[2], Bx[2]);
-	X3 = B[3] = _mm_xor_si128(B[3], Bx[3]);
-
-	for (i = 0; i < 8; i += 2) {
-		/* Operate on "columns". */
-		T = _mm_add_epi32(X0, X3);
-		X1 = _mm_xor_si128(X1, _mm_slli_epi32(T, 7));
-		X1 = _mm_xor_si128(X1, _mm_srli_epi32(T, 25));
-		T = _mm_add_epi32(X1, X0);
-		X2 = _mm_xor_si128(X2, _mm_slli_epi32(T, 9));
-		X2 = _mm_xor_si128(X2, _mm_srli_epi32(T, 23));
-		T = _mm_add_epi32(X2, X1);
-		X3 = _mm_xor_si128(X3, _mm_slli_epi32(T, 13));
-		X3 = _mm_xor_si128(X3, _mm_srli_epi32(T, 19));
-		T = _mm_add_epi32(X3, X2);
-		X0 = _mm_xor_si128(X0, _mm_slli_epi32(T, 18));
-		X0 = _mm_xor_si128(X0, _mm_srli_epi32(T, 14));
-
-		/* Rearrange data. */
-		X1 = _mm_shuffle_epi32(X1, 0x93);
-		X2 = _mm_shuffle_epi32(X2, 0x4E);
-		X3 = _mm_shuffle_epi32(X3, 0x39);
-
-		/* Operate on "rows". */
-		T = _mm_add_epi32(X0, X1);
-		X3 = _mm_xor_si128(X3, _mm_slli_epi32(T, 7));
-		X3 = _mm_xor_si128(X3, _mm_srli_epi32(T, 25));
-		T = _mm_add_epi32(X3, X0);
-		X2 = _mm_xor_si128(X2, _mm_slli_epi32(T, 9));
-		X2 = _mm_xor_si128(X2, _mm_srli_epi32(T, 23));
-		T = _mm_add_epi32(X2, X3);
-		X1 = _mm_xor_si128(X1, _mm_slli_epi32(T, 13));
-		X1 = _mm_xor_si128(X1, _mm_srli_epi32(T, 19));
-		T = _mm_add_epi32(X1, X2);
-		X0 = _mm_xor_si128(X0, _mm_slli_epi32(T, 18));
-		X0 = _mm_xor_si128(X0, _mm_srli_epi32(T, 14));
-
-		/* Rearrange data. */
-		X1 = _mm_shuffle_epi32(X1, 0x39);
-		X2 = _mm_shuffle_epi32(X2, 0x4E);
-		X3 = _mm_shuffle_epi32(X3, 0x93);
-	}
-
-	B[0] = _mm_add_epi32(B[0], X0);
-	B[1] = _mm_add_epi32(B[1], X1);
-	B[2] = _mm_add_epi32(B[2], X2);
-	B[3] = _mm_add_epi32(B[3], X3);
-}
-
-void scrypt_1024_1_1_256_sp(const char *input, char *output, char *scratchpad)
-{
-	uint8_t B[128];
-	__m128i X[8];
-	__m128i *V;
-	uint32_t i, j, k;
-
-	V = (__m128i *)(((uintptr_t)(scratchpad) + 63) & ~ (uintptr_t)(63));
-
-	PBKDF2_SHA256((const uint8_t *)input, 80, (const uint8_t *)input, 80, 1, B, 128);
-
-	for (k = 0; k < 2; k++) {
-		for (i = 0; i < 16; i++) {
-			((uint32_t *)X)[k * 16 + i] = le32dec(&B[(k * 16 + (i * 5 % 16)) * 4]);
-		}
-	}
-
-	for (i = 0; i < 1024; i++) {
-		for (k = 0; k < 8; k++)
-			((__m128i *)V)[i * 8 + k] = X[k];
-		xor_salsa8(&X[0], &X[4]);
-		xor_salsa8(&X[4], &X[0]);
-	}
-	for (i = 0; i < 1024; i++) {
-		j = 8 * (((uint32_t *)X)[16] & 1023);
-		for (k = 0; k < 8; k++)
-			X[k] = _mm_xor_si128(X[k], V[j + k]);
-		xor_salsa8(&X[0], &X[4]);
-		xor_salsa8(&X[4], &X[0]);
-	}
-
-	for (k = 0; k < 2; k++) {
-		for (i = 0; i < 16; i++) {
-			le32enc(&B[(k * 16 + (i * 5 % 16)) * 4], ((uint32_t *)X)[k * 16 + i]);
-		}
-	}
-
-	PBKDF2_SHA256((const uint8_t *)input, 80, B, 128, 1, (uint8_t *)output, 32);
-}
-
-#else /* __SSE2__ */
 
 #define ROTL(a, b) (((a) << (b)) | ((a) >> (32 - (b))))
 
@@ -396,11 +275,14 @@ void scrypt_1024_1_1_256_sp(const char *input, char *output, char *scratchpad)
 	PBKDF2_SHA256((const uint8_t *)input, 80, B, 128, 1, (uint8_t *)output, 32);
 }
 
-#endif /* __SSE2__ */
-
-
 void scrypt_1024_1_1_256(const char *input, char *output)
 {
 	char scratchpad[SCRYPT_SCRATCHPAD_SIZE];
-	scrypt_1024_1_1_256_sp(input, output, scratchpad);
+#ifdef USE_SSE2
+        // todo: runtime detection at startup and use function pointer
+        if(1)
+          scrypt_1024_1_1_256_sp_sse2(input, output, scratchpad);
+        else
+#endif
+          scrypt_1024_1_1_256_sp(input, output, scratchpad);
 }
