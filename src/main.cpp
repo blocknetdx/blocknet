a5b819423a0fad66e1cf9ac759ea3835e8e895f2
Evan <evan@evan-ubuntu>
2014-02-13 12:19:24 -0700

    updated for gpu fork

--- src/main.cpp
+++ src/main.cpp
@@ -1085,9 +1085,16 @@ int64 static GetBlockValue(int nBits, int nHeight, int64 nFees)
 
     int64 nSubsidy = 0; 
     if(nHeight >= 5465) {
-        nSubsidy = (11111.0 / (pow((dDiff+51.0)/6.0,2.0)));
-        if (nSubsidy > 500) nSubsidy = 500;
-        if (nSubsidy < 25) nSubsidy = 25;
+        if(dDiff > 75) { // GPU/ASIC difficulty calc
+            // 2222222/(((x+2600)/9)^2)
+            nSubsidy = (2222222.0 / (pow((dDiff+2600.0)/9.0,2.0)));
+            if (nSubsidy > 25) nSubsidy = 25;
+            if (nSubsidy < 5) nSubsidy = 5;
+        } else { // CPU mining calc
+            nSubsidy = (11111.0 / (pow((dDiff+51.0)/6.0,2.0)));
+            if (nSubsidy > 500) nSubsidy = 500;
+            if (nSubsidy < 25) nSubsidy = 25;
+        }
     } else {
         nSubsidy = (1111.0 / (pow((dDiff+1.0),2.0)));
         if (nSubsidy > 500) nSubsidy = 500;
