f4433f8557bfe86063235e7eba46e6da24487989
Evan Duffield <evan@darkcoin.io>
2014-04-29 07:18:15 -0700

    dgw3

--- src/main.h
+++ src/main.h
@@ -3,7 +3,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef BITCOIN_MAIN_H
-#define BITCOIN_MAIN_HC
+#define BITCOIN_MAIN_H
 
 #include "bignum.h"
 #include "sync.h"
