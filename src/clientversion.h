4417f94856d419652ec155fe90d9e734192dcac4
Evan Duffield <evan@darkcoin.io>
2014-05-26 11:18:46 -0700

    version bump

--- src/clientversion.h
+++ src/clientversion.h
@@ -9,7 +9,7 @@
 #define CLIENT_VERSION_MAJOR       0
 #define CLIENT_VERSION_MINOR       9
 #define CLIENT_VERSION_REVISION    4
-#define CLIENT_VERSION_BUILD       8
+#define CLIENT_VERSION_BUILD       9
 
 // Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE  true
