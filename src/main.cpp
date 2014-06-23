e025fe7934074311104bacc35c931094304b4d48
R E Broadley <rebroad+github@gmail.com>
2014-06-23 10:07:45 +0700

    Add masternodes to address database

--- src/main.cpp
+++ src/main.cpp
@@ -4183,6 +4183,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
                 return false;
             }
 
+            addrman.Add(CAddress(addr), pfrom->addr, 2*60*60);
 
             CMasterNode mn(addr, vin, pubkey, vchSig, sigTime, pubkey2);
             mn.UpdateLastSeen(lastUpdated);
