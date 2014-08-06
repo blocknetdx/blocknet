1d3d3613eca1c8d0c173f0a61bfac8db2cce03eb
Evan Duffield <evan@darkcoin.io>
2014-08-06 14:25:06 -0700

    version bump

--- src/main.h
+++ src/main.h
@@ -2583,7 +2583,7 @@ static const int64 DARKSEND_FEE = 0.001*COIN;
 class CDarkSendPool
 {
 public:
-    static const int MIN_PEER_PROTO_VERSION = 70027;
+    static const int MIN_PEER_PROTO_VERSION = 70028;
 
     std::vector<CDarkSendEntry> myEntries;
     std::vector<CDarkSendEntry> entries;
