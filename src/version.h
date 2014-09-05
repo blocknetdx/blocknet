6903fbb3cd8ea7691371921573de4f082d3d3a1c
Evan Duffield <evan@darkcoin.io>
2014-09-05 12:11:46 -0700

    version bump

--- src/version.h
+++ src/version.h
@@ -25,7 +25,7 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 70036;
+static const int PROTOCOL_VERSION = 70037;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
