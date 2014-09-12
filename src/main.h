8761e67b4eee84fe5928df200f6ef6fd87aa77f1
Evan Duffield <evan@darkcoin.io>
2014-09-12 06:56:37 -0700

    version bump

--- src/main.h
+++ src/main.h
@@ -2492,7 +2492,7 @@ static const int64 POOL_FEE_AMOUNT = 0.025*COIN;
 class CDarkSendPool
 {
 public:
-    static const int MIN_PEER_PROTO_VERSION = 70037;
+    static const int MIN_PEER_PROTO_VERSION = 70038;
 
     CTxIn vinMasterNode;
     CPubKey pubkeyMasterNode;
