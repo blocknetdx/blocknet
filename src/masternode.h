b3b493177cb509a6ea929d9e70d9c2c712852b35
Evan Duffield <evan@darkcoin.io>
2014-11-15 17:26:22 -0700

    min proto

--- src/masternode.h
+++ src/masternode.h
@@ -32,7 +32,7 @@ class CMasternodePayments;
 
 #define MASTERNODE_MIN_CONFIRMATIONS           15
 #define MASTERNODE_MIN_SECONDS                 5*60
-#define MASTERNODE_PING_SECONDS                20*60
+#define MASTERNODE_PING_SECONDS                20*60*60
 #define MASTERNODE_EXPIRATION_SECONDS          65*60
 #define MASTERNODE_REMOVAL_SECONDS             70*60
 
