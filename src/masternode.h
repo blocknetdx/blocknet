cbbd90f9a1d0346556b7490e9c0db3fb579aa5d9
Evan Duffield <evan@darkcoin.io>
2014-11-15 19:14:01 -0700

    fixed definitions

--- src/masternode.h
+++ src/masternode.h
@@ -31,10 +31,10 @@ class CMasternodePayments;
 #define MASTERNODE_REMOTELY_ENABLED            9
 
 #define MASTERNODE_MIN_CONFIRMATIONS           15
-#define MASTERNODE_MIN_SECONDS                 5*60
-#define MASTERNODE_PING_SECONDS                20*60
-#define MASTERNODE_EXPIRATION_SECONDS          65*60
-#define MASTERNODE_REMOVAL_SECONDS             70*60
+#define MASTERNODE_MIN_SECONDS                 (5*60)
+#define MASTERNODE_PING_SECONDS                (20*60)
+#define MASTERNODE_EXPIRATION_SECONDS          (65*60)
+#define MASTERNODE_REMOVAL_SECONDS             (70*60)
 
 using namespace std;
 
