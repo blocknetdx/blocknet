6577fa57f33ee4e69c430aa46361d2c668ad618a
Evan Duffield <evan@darkcoin.io>
2014-12-01 09:43:52 -0700

    increased min seconds

--- src/masternode.h
+++ src/masternode.h
@@ -31,7 +31,7 @@ class CMasternodePayments;
 #define MASTERNODE_REMOTELY_ENABLED            9
 
 #define MASTERNODE_MIN_CONFIRMATIONS           15
-#define MASTERNODE_MIN_SECONDS                 (5*60)
+#define MASTERNODE_MIN_SECONDS                 (20*60)
 #define MASTERNODE_PING_SECONDS                (5*60)
 #define MASTERNODE_EXPIRATION_SECONDS          (65*60)
 #define MASTERNODE_REMOVAL_SECONDS             (70*60)
