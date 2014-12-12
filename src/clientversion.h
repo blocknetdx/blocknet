f2cf90d7e16c3d9d383774f0141dfc94a0c670d5
Evan Duffield <evan@darkcoin.io>
2014-12-12 07:26:36 -0700

    version bump

--- src/clientversion.h
+++ src/clientversion.h
@@ -9,12 +9,12 @@
 #define CLIENT_VERSION_MAJOR       0
 #define CLIENT_VERSION_MINOR       10
 #define CLIENT_VERSION_REVISION    17
-#define CLIENT_VERSION_BUILD       23
+#define CLIENT_VERSION_BUILD       24
 
 // Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE  true
 
-// Copyright year (2009-this)		
+// Copyright year (2009-this)
 // Todo: update this when changing our copyright comments in the source
 #define COPYRIGHT_YEAR 2013
 
