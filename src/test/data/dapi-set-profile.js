f8f8a945a12b5b4948438b6c9096062b805f7d97
Evan Duffield <evan@dashpay.io>
2015-12-16 09:41:38 -0700

    set-profile-js

---
+++ src/test/data/dapi-set-profile.js
@@ -0,0 +1,12 @@
+{
+    "object" : "dapi_command",
+    "data" : {
+        "command": "set-profile",
+        "my_uid": "1337",
+        "target_uid": "1337", 
+        "signature": "",
+        "update" : [
+            {"field":"name","value":"uberevan"}
+        ]
+    }
+}
