272c1acd1e9f8fc4f0281c7b8aac98ea5a59956d
Evan Duffield <evan@dashpay.io>
2015-12-16 10:40:20 -0700

    get and set private data js

---
+++ src/test/data/dapi-get-private-data.js
@@ -0,0 +1,10 @@
+{   
+    "object" : "dapi_command",
+    "data" : {
+        "command" : "get-private-data",
+        "my_uid" : "1337",
+        "target_uid" : "1337", 
+        "signature" : "SIG",
+        "slot" : 1
+    }
+}
---
+++ src/test/data/dapi-set-private-data.js
@@ -0,0 +1,11 @@
+{   
+    "object" : "dapi_command",
+    "data" : {
+        "command" : "set-private-data",
+        "my_uid" : "1337",
+        "target_uid" : "1337", 
+        "signature" : "SIG",
+        "slot" : 1,
+        "payload" : "AOEU"
+    }
+}
