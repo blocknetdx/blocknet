841cd44e697ace3d17da8e2eb3aabe14b7f6b97e
NicolasDorier <nicolas.dorier@gmail.com>
2016-04-10 15:59:23 +0900

    signrawtransaction can sign P2WSH

--- src/script/script.h
+++ src/script/script.h
@@ -593,6 +593,7 @@ public:
     bool IsZerocoinMint() const;
     bool IsZerocoinSpend() const;
     
+    bool IsPayToWitnessScriptHash() const;
     bool IsWitnessProgram(int& version, std::vector<unsigned char>& program) const;
 
     /** Called by IsStandardTx and P2SH/BIP62 VerifyScript (which makes it consensus-critical). */
