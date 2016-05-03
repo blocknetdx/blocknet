53a9bdc1fa92a56e6b25b74c6902d8fe07dfec53
StakeBox <support@stakebox.com>
2016-05-03 17:26:43 -0400

    added seed nodes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -138,11 +138,9 @@ public:
         assert(hashGenesisBlock == uint256("0x0000041e482b9b9691d98eefb48473405c0b8ec31b76df3797c74a78680ef818"));
         assert(genesis.hashMerkleRoot == uint256("0x1b2ef6e2f28be914103a277377ae7729dcd125dfeb8bf97bd5964ba72b6dc39b"));
 
-        vSeeds.push_back(CDNSSeedData("dnsseed.darknet-crypto.com", ""));
-        vSeeds.push_back(CDNSSeedData("darknetexporer.coin-server.com", ""));
-        vSeeds.push_back(CDNSSeedData("darknet.coin-server.com", "")); 
+        vSeeds.push_back(CDNSSeedData("coin-server.com", ""));
+        vSeeds.push_back(CDNSSeedData("173.245.158.8", ""));
         vSeeds.push_back(CDNSSeedData("162.213.154.31", ""));
-        vSeeds.push_back(CDNSSeedData("173.245.148.70", ""));
         vSeeds.push_back(CDNSSeedData("151.80.206.104", ""));
 
         base58Prefixes[PUBKEY_ADDRESS] = list_of( 30);                    
