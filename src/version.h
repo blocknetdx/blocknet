16b48ddc38876e7fec9383434b85bd0475859bd4
StakeBox <support@stakebox.com>
2016-08-02 10:34:50 -0400

    updated protocol

--- src/version.h
+++ src/version.h
@@ -24,7 +24,7 @@ static const int MIN_PEER_PROTO_VERSION = 70106;
 static const int MIN_PEER_PROTO_VERSION_POS = 70199;
 
 //! minimum peer version accepted by ObfuscationPool
-static const int MIN_POOL_PEER_PROTO_VERSION = 70107;
+static const int MIN_POOL_PEER_PROTO_VERSION = 70200;
 
 //! minimum peer version for masternode budgets
 static const int MIN_BUDGET_PEER_PROTO_VERSION = 70106;
@@ -35,8 +35,8 @@ static const int MIN_MNW_PEER_PROTO_VERSION = 70104;
 //! minimum peer version that can receive masternode payments
 // V1 - Last protocol version before update
 // V2 - Newest protocol version
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 70106;
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 70107;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 70107;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 70200;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
