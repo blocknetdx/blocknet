9553b62efa9bb75828690c797ae9b42600f30b4b
StakeBox <support@stakebox.com>
2016-09-06 23:58:35 -0400

    add stake and masternode rewards to immature balance on main overview page

--- src/wallet.h
+++ src/wallet.h
@@ -855,7 +855,7 @@ public:
 
     CAmount GetImmatureCredit(bool fUseCache=true) const
     {
-        if (IsCoinBase() && GetBlocksToMaturity() > 0 && IsInMainChain())
+        if ((IsCoinBase() || IsCoinStake()) && GetBlocksToMaturity() > 0 && IsInMainChain())
         {
             if (fUseCache && fImmatureCreditCached)
                 return nImmatureCreditCached;
