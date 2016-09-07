bac075f8f4df2eb25b9596898fa16f4659bf9851
StakeBox <support@stakebox.com>
2016-09-07 19:35:09 -0400

    fix masternode rewards when number of masternodes is equal to a seesaw step

--- src/main.cpp
+++ src/main.cpp
@@ -1683,46 +1683,46 @@ int64_t GetMasternodePayment(int nHeight, int64_t blockValue)
         else if (mNodeCoins <= (nMoneySupply * .05) && mNodeCoins > 0) {
             ret = blockValue * .85;
         }
-        else if (mNodeCoins <= (nMoneySupply * .1) && mNodeCoins >= (nMoneySupply * .05)) {
+        else if (mNodeCoins <= (nMoneySupply * .1) && mNodeCoins > (nMoneySupply * .05)) {
             ret = blockValue * .8;
         }
-        else if (mNodeCoins <= (nMoneySupply * .15) && mNodeCoins >= (nMoneySupply * .1)) {
+        else if (mNodeCoins <= (nMoneySupply * .15) && mNodeCoins > (nMoneySupply * .1)) {
             ret = blockValue * .75;
         }
-        else if (mNodeCoins <= (nMoneySupply * .2) && mNodeCoins >= (nMoneySupply * .15)) {
+        else if (mNodeCoins <= (nMoneySupply * .2) && mNodeCoins > (nMoneySupply * .15)) {
             ret = blockValue * .7;
         }
-        else if (mNodeCoins <= (nMoneySupply * .25) && mNodeCoins >= (nMoneySupply * .2)) {
+        else if (mNodeCoins <= (nMoneySupply * .25) && mNodeCoins > (nMoneySupply * .2)) {
             ret = blockValue * .65;
         }
-        else if (mNodeCoins <= (nMoneySupply * .3) && mNodeCoins >= (nMoneySupply * .25)) {
+        else if (mNodeCoins <= (nMoneySupply * .3) && mNodeCoins > (nMoneySupply * .25)) {
             ret = blockValue * .6;
         }
-        else if (mNodeCoins <= (nMoneySupply * .35) && mNodeCoins >= (nMoneySupply * .3)) {
+        else if (mNodeCoins <= (nMoneySupply * .35) && mNodeCoins > (nMoneySupply * .3)) {
             ret = blockValue * .55;
         }
-        else if (mNodeCoins <= (nMoneySupply * .4) && mNodeCoins >= (nMoneySupply * .35)) {
+        else if (mNodeCoins <= (nMoneySupply * .4) && mNodeCoins > (nMoneySupply * .35)) {
             ret = blockValue * .5;
         }
-        else if (mNodeCoins <= (nMoneySupply * .45) && mNodeCoins >= (nMoneySupply * .4)) {
+        else if (mNodeCoins <= (nMoneySupply * .45) && mNodeCoins > (nMoneySupply * .4)) {
             ret = blockValue * .45;
         }
-        else if (mNodeCoins <= (nMoneySupply * .5) && mNodeCoins >= (nMoneySupply * .45)) {
+        else if (mNodeCoins <= (nMoneySupply * .5) && mNodeCoins > (nMoneySupply * .45)) {
             ret = blockValue * .4;
         }
-        else if (mNodeCoins <= (nMoneySupply * .55) && mNodeCoins >= (nMoneySupply * .5)) {
+        else if (mNodeCoins <= (nMoneySupply * .55) && mNodeCoins > (nMoneySupply * .5)) {
             ret = blockValue * .35;
         }
-        else if (mNodeCoins <= (nMoneySupply * .6) && mNodeCoins >= (nMoneySupply * .55)) {
+        else if (mNodeCoins <= (nMoneySupply * .6) && mNodeCoins > (nMoneySupply * .55)) {
             ret = blockValue * .3;
         }
-        else if (mNodeCoins <= (nMoneySupply * .65) && mNodeCoins >= (nMoneySupply * .6)) {
+        else if (mNodeCoins <= (nMoneySupply * .65) && mNodeCoins > (nMoneySupply * .6)) {
             ret = blockValue * .25;
         }
-        else if (mNodeCoins <= (nMoneySupply * .7) && mNodeCoins >= (nMoneySupply * .65)) {
+        else if (mNodeCoins <= (nMoneySupply * .7) && mNodeCoins > (nMoneySupply * .65)) {
             ret = blockValue * .2;
         }
-        else if (mNodeCoins <= (nMoneySupply * .75) && mNodeCoins >= (nMoneySupply * .7)) {
+        else if (mNodeCoins <= (nMoneySupply * .75) && mNodeCoins > (nMoneySupply * .7)) {
             ret = blockValue * .15;
         }
         else{
