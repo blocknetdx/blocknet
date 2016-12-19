d47da026b562a79b74aa69e666d838c80d5cd9bf
Mrs-X <Mrs-X@users.noreply.github.com>
2016-12-19 11:10:16 +0100

    Gitian for Linux fixes 2

--- src/compat/glibcxx_compat.cpp
+++ src/compat/glibcxx_compat.cpp
@@ -2,28 +2,60 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#if defined(HAVE_CONFIG_H)
-#include "config/darknet-config.h"
-#endif
+#include <list>
+#include <locale>
+#include <stdexcept>
 
-#include <cstddef>
-
-#if defined(HAVE_SYS_SELECT_H)
-#include <sys/select.h>
-#endif
-
-// Prior to GLIBC_2.14, memcpy was aliased to memmove.
-extern "C" void* memmove(void* a, const void* b, size_t c);
-extern "C" void* memcpy(void* a, const void* b, size_t c)
+namespace
 {
-    return memmove(a, b, c);
+// trigger: use ctype<char>::widen to trigger ctype<char>::_M_widen_init().
+// test: convert a char from narrow to wide and back. Verify that the result
+//   matches the original.
+bool sanity_test_widen(char testchar)
+{
+    const std::ctype<char>& test(std::use_facet<std::ctype<char> >(std::locale()));
+    return test.narrow(test.widen(testchar), 'b') == testchar;
 }
 
-extern "C" void __chk_fail(void) __attribute__((__noreturn__));
-extern "C" FDELT_TYPE __fdelt_warn(FDELT_TYPE a)
+// trigger: use list::push_back and list::pop_back to trigger _M_hook and
+//   _M_unhook.
+// test: Push a sequence of integers into a list. Pop them off and verify that
+//   they match the original sequence.
+bool sanity_test_list(unsigned int size)
 {
-    if (a >= FD_SETSIZE)
-        __chk_fail();
-    return a / __NFDBITS;
+    std::list<unsigned int> test;
+    for (unsigned int i = 0; i != size; ++i)
+        test.push_back(i + 1);
+
+    if (test.size() != size)
+        return false;
+
+    while (!test.empty()) {
+        if (test.back() != test.size())
+            return false;
+        test.pop_back();
+    }
+    return true;
 }
-extern "C" FDELT_TYPE __fdelt_chk(FDELT_TYPE) __attribute__((weak, alias("__fdelt_warn")));
+
+} // anon namespace
+
+// trigger: string::at(x) on an empty string to trigger __throw_out_of_range_fmt.
+// test: force std::string to throw an out_of_range exception. Verify that
+//   it's caught correctly.
+bool sanity_test_range_fmt()
+{
+    std::string test;
+    try {
+        test.at(1);
+    } catch (const std::out_of_range&) {
+        return true;
+    } catch (...) {
+    }
+    return false;
+}
+
+bool glibcxx_sanity_test()
+{
+    return sanity_test_widen('a') && sanity_test_list(100) && sanity_test_range_fmt();
+}
