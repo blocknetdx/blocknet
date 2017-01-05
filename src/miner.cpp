e569b6fcfaf8e2e94d6e6420cd4586e8bc209eac
presstab <presstab1337@gmail.com>
2017-01-05 10:28:08 -0700

    undo testnet only miner code

--- src/miner.cpp
+++ src/miner.cpp
@@ -494,7 +494,7 @@ void BitcoinMiner(CWallet *pwallet, bool fProofOfStake)
                 continue;
             }
 
-            while (chainActive.Tip()->nTime < 1471482000 /*|| vNodes.empty()*/ || pwallet->IsLocked() ||  !fMintableCoins || nReserveBalance >= pwallet->GetBalance() /*|| !masternodeSync.IsSynced()*/)
+            while (chainActive.Tip()->nTime < 1471482000 || vNodes.empty() || pwallet->IsLocked() ||  !fMintableCoins || nReserveBalance >= pwallet->GetBalance() || !masternodeSync.IsSynced())
             {
                 nLastCoinStakeSearchInterval = 0;
                 MilliSleep(5000);
