a92fa3cef05b43ad0d5b633322a2c6de0c8c467d
Jon Spock <jonspock@protonmail.com>
2017-04-11 19:45:27 -0700

    Some manual formatting to override clang-format

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -52,7 +52,9 @@ static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data
 //    timestamp before)
 // + Contains no strange transactions
 static Checkpoints::MapCheckpoints mapCheckpoints =
-    boost::assign::map_list_of(259201, uint256("1c9121bf9329a6234bfd1ea2d91515f19cd96990725265253f4b164283ade5dd"))(424998, uint256("f31e381eedb0ed3ed65fcc98cc71f36012bee32e8efd017c4f9fb0620fd35f6b"))(593791, uint256("3c9d06ca180bb81aa3b488e160c1c86fa22c21d02e5c400043e30939a08300d7"));
+    boost::assign::map_list_of(259201, uint256("1c9121bf9329a6234bfd1ea2d91515f19cd96990725265253f4b164283ade5dd")) ///
+    (424998, uint256("f31e381eedb0ed3ed65fcc98cc71f36012bee32e8efd017c4f9fb0620fd35f6b"))                           ///
+    (593791, uint256("3c9d06ca180bb81aa3b488e160c1c86fa22c21d02e5c400043e30939a08300d7"));
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
     1491835916, // * UNIX timestamp of last checkpoint block
