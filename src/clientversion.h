96e8cc5f2340c46d7748aaabd8eac239e0de3668
Fuzzbawls <fuzzbawls@gmail.com>
2017-04-21 14:02:59 -0700

    [Build] Bump master to 2.2.99
    Now that 2.2 has been branched off, set master to 2.2.99 (pre-2.3).

--- src/clientversion.h
+++ src/clientversion.h
@@ -15,7 +15,7 @@
 
 //! These need to be macros, as clientversion.cpp's and pivx*-res.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR 2
-#define CLIENT_VERSION_MINOR 1
+#define CLIENT_VERSION_MINOR 2
 #define CLIENT_VERSION_REVISION 99
 #define CLIENT_VERSION_BUILD 0
 
