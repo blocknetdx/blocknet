afae0f690132b3d74ed24ac73dbb18e7f12d7d93
presstab <presstab1337@gmail.com>
2017-06-04 11:58:01 -0600

    Add missing zerocoin.cpp from last commit

---
+++ src/primitives/zerocoin.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2017 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "zerocoin.h"
+#include "hash.h"
+#include "utilstrencodings.h"
+
+uint256 CZerocoinMint::GetHash() const
+{
+    return Hash(BEGIN(value), END(value));
+}
+
+uint256 CZerocoinSpend::GetHash() const
+{
+    return Hash(BEGIN(coinSerial), END(coinSerial));
+}
+
