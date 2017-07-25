b25988ba634f838bf3d5e36405aba73a855b2760
atcsecure <atcsecure@xcpool.xcurrency.co>
2017-07-25 18:27:02 -0400

    cparams dev update

--- src/main.cpp
+++ src/main.cpp
@@ -2399,13 +2399,14 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
     nTimeConnect += nTime1 - nTimeStart;
     LogPrint("bench", "      - Connect %u transactions: %.2fms (%.3fms/tx, %.3fms/txin) [%.2fs]\n", (unsigned)block.vtx.size(), 0.001 * (nTime1 - nTimeStart), 0.001 * (nTime1 - nTimeStart) / block.vtx.size(), nInputs <= 1 ? 0 : 0.001 * (nTime1 - nTimeStart) / (nInputs - 1), nTimeConnect * 0.000001);
 
+    /* disabled
     if (!IsInitialBlockDownload() && !IsBlockValueValid(block, GetBlockValue(pindex->pprev->nHeight))) {
         return state.DoS(100,
             error("ConnectBlock() : reward pays too much (actual=%d vs limit=%d)",
                 block.vtx[0].GetValueOut(), GetBlockValue(pindex->pprev->nHeight)),
             REJECT_INVALID, "bad-cb-amount");
     }
-
+    */
     if (!control.Wait())
         return state.DoS(100, false);
     int64_t nTime2 = GetTimeMicros();
