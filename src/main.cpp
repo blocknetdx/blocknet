e7c279c9dbb404f2043da2f167e2bc6b3c760bbc
atcsecure <dmetcalf@atcsecure.com>
2017-08-24 08:19:21 -0400

    fixed code error

--- src/main.cpp
+++ src/main.cpp
@@ -1627,9 +1627,9 @@ int64_t GetBlockValue(int nHeight)
         nSubsidy = 4060024 * COIN;
     } else if (nHeight < 525600 && nHeight > 0) {
 	nSubsidy = 1 * COIN;
-    } else if (nHeight <= 1051200 && nHeight => 525600) {
+    } else if (nHeight <= 1051200 && nHeight >= 525600) {
 	nSubsidy = 0.5 * COIN;
-    } else if (nHeight => 1051201) {
+    } else if (nHeight >= 1051201) {
 	nSubsidy = 0.25 * COIN;
     } else {
         nSubsidy = 1 * COIN;
