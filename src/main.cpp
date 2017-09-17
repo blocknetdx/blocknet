2a83e103fea1ad5f0fde0cf4c768d8a74016024c
ATC <atcsecure@users.noreply.github.com>
2017-09-17 13:49:36 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2014-2015 The Dash developers
 // Copyright (c) 2015-2017 The BlocknetDX developers
+// Copyright (c) 2014-2017 PPCoin Developers
+// Copyright (c) 2017 PIVX Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
