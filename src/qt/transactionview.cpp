84676584bc55eb0a74ade22542f08ffe12a592f9
Mrs-X <Mrs-X@users.noreply.github.com>
2017-10-08 17:22:37 +0200

    Check if model is valid before using in a couple of places in transactionView

--- src/qt/transactionview.cpp
+++ src/qt/transactionview.cpp
@@ -332,14 +332,16 @@ void TransactionView::changedAmount(const QString& amount)
         return;
     CAmount amount_parsed = 0;
 
-    // Replace "," by "." so BitcoinUnits::parse will not fail for users entering "," as decimal separator
-    QString newAmount = amount;
-    newAmount.replace(QString(","), QString("."));
+    if (model) {
+        // Replace "," by "." so BitcoinUnits::parse will not fail for users entering "," as decimal separator
+        QString newAmount = amount;
+        newAmount.replace(QString(","), QString("."));
 
-    if (BitcoinUnits::parse(model->getOptionsModel()->getDisplayUnit(), newAmount, &amount_parsed)) {
-        transactionProxyModel->setMinAmount(amount_parsed);
-    } else {
-        transactionProxyModel->setMinAmount(0);
+        if (BitcoinUnits::parse(model->getOptionsModel()->getDisplayUnit(), newAmount, &amount_parsed)) {
+            transactionProxyModel->setMinAmount(amount_parsed);
+        } else {
+            transactionProxyModel->setMinAmount(0);
+        }
     }
 }
 
@@ -354,25 +356,31 @@ void TransactionView::exportClicked()
         return;
 
     CSVModelWriter writer(filename);
+    bool fExport = false;
 
-    // name, column, role
-    writer.setModel(transactionProxyModel);
-    writer.addColumn(tr("Confirmed"), 0, TransactionTableModel::ConfirmedRole);
-    if (model && model->haveWatchOnly())
-        writer.addColumn(tr("Watch-only"), TransactionTableModel::Watchonly);
-    writer.addColumn(tr("Date"), 0, TransactionTableModel::DateRole);
-    writer.addColumn(tr("Type"), TransactionTableModel::Type, Qt::EditRole);
-    writer.addColumn(tr("Label"), 0, TransactionTableModel::LabelRole);
-    writer.addColumn(tr("Address"), 0, TransactionTableModel::AddressRole);
-    writer.addColumn(BitcoinUnits::getAmountColumnTitle(model->getOptionsModel()->getDisplayUnit()), 0, TransactionTableModel::FormattedAmountRole);
-    writer.addColumn(tr("ID"), 0, TransactionTableModel::TxIDRole);
+    if (model) {
+        // name, column, role
+        writer.setModel(transactionProxyModel);
+        writer.addColumn(tr("Confirmed"), 0, TransactionTableModel::ConfirmedRole);
+        if (model->haveWatchOnly())
+            writer.addColumn(tr("Watch-only"), TransactionTableModel::Watchonly);
+        writer.addColumn(tr("Date"), 0, TransactionTableModel::DateRole);
+        writer.addColumn(tr("Type"), TransactionTableModel::Type, Qt::EditRole);
+        writer.addColumn(tr("Label"), 0, TransactionTableModel::LabelRole);
+        writer.addColumn(tr("Address"), 0, TransactionTableModel::AddressRole);
+        writer.addColumn(BitcoinUnits::getAmountColumnTitle(model->getOptionsModel()->getDisplayUnit()), 0, TransactionTableModel::FormattedAmountRole);
+        writer.addColumn(tr("ID"), 0, TransactionTableModel::TxIDRole);
 
-    if (!writer.write()) {
-        emit message(tr("Exporting Failed"), tr("There was an error trying to save the transaction history to %1.").arg(filename),
-            CClientUIInterface::MSG_ERROR);
-    } else {
+        fExport = writer.write();
+    }
+
+    if (fExport) {
         emit message(tr("Exporting Successful"), tr("The transaction history was successfully saved to %1.").arg(filename),
-            CClientUIInterface::MSG_INFORMATION);
+                     CClientUIInterface::MSG_INFORMATION);
+    } 
+    else {
+        emit message(tr("Exporting Failed"), tr("There was an error trying to save the transaction history to %1.").arg(filename),
+                     CClientUIInterface::MSG_ERROR);
     }
 }
 
