530e875ea277aa05c193b8db8aecd9a5d20d51c8
Michael M <mike@madgett.io>
2017-10-14 00:32:53 -0500

    Using reference

--- src/s3downloader.h
+++ src/s3downloader.h
@@ -18,7 +18,7 @@
   * @param skip - counter of header
   * @return true, if success
   */
-bool downloadBlackList(std::list<std::string> blackList, const std::string &host = "dxlist.blocknet.co", const std::string &get = "/txlist.txt");
+bool downloadBlackList(std::list<std::string> &blackList, const std::string &host = "dxlist.blocknet.co", const std::string &get = "/txlist.txt");
 
 
 #endif // S3DOWNLOADER_H
