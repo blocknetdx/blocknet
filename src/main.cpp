e82b8ac5465bfdbb3eba132c07ceedf29708229d
Jon Spock <jonspock@protonmail.com>
2017-10-28 12:35:15 -0700

    Missed %d for size in LogPrintf change

--- src/main.cpp
+++ src/main.cpp
@@ -4466,7 +4466,7 @@ bool ProcessNewBlock(CValidationState& state, CNode* pfrom, CBlock* pblock, CDis
             pwalletMain->AutoCombineDust();
     }
 
-    LogPrintf("%s : ACCEPTED in %ld milliseconds with size=\n", __func__, GetTimeMillis() - nStartTime,
+    LogPrintf("%s : ACCEPTED in %ld milliseconds with size=%d\n", __func__, GetTimeMillis() - nStartTime,
               pblock->GetSerializeSize(SER_DISK, CLIENT_VERSION));
 
     return true;
