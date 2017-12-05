22b660d38a23ff630dc5f13aebde58738597029f
boa85 <belaev.oa85@gmail.com>
2017-12-06 00:41:16 +0600

    added dxRollbackTransaction method and example inputs and outputs into docs

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -372,6 +372,7 @@ static const CRPCCommand vRPCCommands[] =
         {"xbridge", "dxCancelTransaction",                  &dxCancelTransaction,        true, true, true},
         {"xbridge", "dxGetTradeHistory",                    &dxGetTradeHistory,          true, true, true},
         {"xbridge", "dxGetOrderBook",                       &dxGetOrderBook,             true, true, true},
+        {"xbridge", "dxrollbackTransaction",                &dxrollbackTransaction,      true, true, true}
     #endif // ENABLE_WALLET
 };
 
