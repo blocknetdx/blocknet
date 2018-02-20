bcad0c31a5e5853e6ffae9b2aea44d0160a73438
corvaxx <maybe.mail@inbox.ru>
2018-02-20 16:09:50 +0300

    rm unused code

--- src/xbridge/rpcxbridge.cpp
+++ src/xbridge/rpcxbridge.cpp
@@ -697,85 +697,8 @@ Value dxCancelOrder(const Array &params, bool fHelp)
     LOG() << "rpc cancel order " << __FUNCTION__;
     uint256 id(params[0].get_str());
 
-    if (xbridge::App::instance().transactions().count(id) ) {
-        xbridge::TransactionDescrPtr tx = xbridge::App::instance().transaction(id);
-
-        xbridge::WalletConnectorPtr connFrom = xbridge::App::instance().connectorByCurrency(tx->fromCurrency);
-        xbridge::WalletConnectorPtr connTo   = xbridge::App::instance().connectorByCurrency(tx->toCurrency);
-
-        if (!connFrom || !connTo)
-        {
-            Object error;
-            error.emplace_back(Pair("error", xbridge::xbridgeErrorText(xbridge::NO_SESSION)));
-            error.emplace_back(Pair("code", xbridge::NO_SESSION));
-            error.emplace_back(Pair("name", "dxCancelOrder"));
-            return error;
-        }
-
-        if(tx->state < xbridge::TransactionDescr::trCreated)
-        {
-            const auto res = xbridge::App::instance().cancelXBridgeTransaction(id, crRpcRequest);
-            if (res == xbridge::SUCCESS)
-            {
-                Object obj;
-                obj.emplace_back(Pair("id", id.GetHex()));
-
-                obj.emplace_back(Pair("maker", tx->fromCurrency));
-                obj.emplace_back(Pair("maker_size", xBridgeValueFromAmount(tx->fromAmount)));
-                obj.emplace_back(Pair("maker_address", connFrom->fromXAddr(tx->from)));
-
-                obj.emplace_back(Pair("taker", tx->toCurrency));
-                obj.emplace_back(Pair("taker_size", xBridgeValueFromAmount(tx->toAmount)));
-                obj.emplace_back(Pair("taker_address", connTo->fromXAddr(tx->to)));
-
-                obj.emplace_back(Pair("updated_at", bpt::to_iso_extended_string(tx->txtime)));
-                obj.emplace_back(Pair("created_at", bpt::to_iso_extended_string(tx->created)));
-
-                obj.emplace_back(Pair("status", tx->strState()));
-                return obj;
-            }
-            else
-            {
-                Object obj;
-                obj.emplace_back(Pair("error", xbridge::xbridgeErrorText(res)));
-                obj.emplace_back(Pair("code", res));
-                obj.emplace_back(Pair("name", "dxCancelOrder"));
-                return obj;
-            }
-        }
-        else
-        {
-            const auto res = xbridge::App::instance().rollbackXBridgeTransaction(id);
-            if (res == xbridge::SUCCESS)
-            {
-                Object obj;
-                obj.emplace_back(Pair("id", id.GetHex()));
-
-                obj.emplace_back(Pair("maker", tx->fromCurrency));
-                obj.emplace_back(Pair("maker_size", xBridgeValueFromAmount(tx->fromAmount)));
-                obj.emplace_back(Pair("maker_address", connFrom->fromXAddr(tx->from)));
-
-                obj.emplace_back(Pair("taker", tx->toCurrency));
-                obj.emplace_back(Pair("taker_size", xBridgeValueFromAmount(tx->toAmount)));
-                obj.emplace_back(Pair("taker_address", connTo->fromXAddr(tx->to)));
-
-                obj.emplace_back(Pair("updated_at", bpt::to_iso_extended_string(tx->txtime)));
-                obj.emplace_back(Pair("created_at", bpt::to_iso_extended_string(tx->created)));
-
-                obj.emplace_back(Pair("status", tx->strState()));
-                return obj;
-            }
-            else
-            {
-                Object obj;
-                obj.emplace_back(Pair("error", xbridge::xbridgeErrorText(res)));
-                obj.emplace_back(Pair("code", res));
-                obj.emplace_back(Pair("name", "dxCancelOrder"));
-                return obj;
-            }
-        }
-    }
-    else
+    xbridge::TransactionDescrPtr tx = xbridge::App::instance().transaction(id);
+    if (!tx)
     {
         Object obj;
         obj.emplace_back(Pair("error", xbridge::xbridgeErrorText(xbridge::Error::TRANSACTION_NOT_FOUND, id.GetHex())));
@@ -783,6 +706,53 @@ Value dxCancelOrder(const Array &params, bool fHelp)
         obj.emplace_back(Pair("name", "dxCancelOrder"));
         return obj;
     }
+
+    if (tx->state >= xbridge::TransactionDescr::trCreated)
+    {
+        Object error;
+        error.emplace_back(Pair("error", xbridge::xbridgeErrorText(xbridge::INVALID_STATE)));
+        error.emplace_back(Pair("code", xbridge::INVALID_STATE));
+        error.emplace_back(Pair("name", "dxCancelOrder"));
+        return error;
+    }
+
+    const auto res = xbridge::App::instance().cancelXBridgeTransaction(id, crRpcRequest);
+    if (res != xbridge::SUCCESS)
+    {
+        Object obj;
+        obj.emplace_back(Pair("error", xbridge::xbridgeErrorText(res)));
+        obj.emplace_back(Pair("code", res));
+        obj.emplace_back(Pair("name", "dxCancelOrder"));
+        return obj;
+    }
+
+    xbridge::WalletConnectorPtr connFrom = xbridge::App::instance().connectorByCurrency(tx->fromCurrency);
+    xbridge::WalletConnectorPtr connTo   = xbridge::App::instance().connectorByCurrency(tx->toCurrency);
+    if (!connFrom || !connTo)
+    {
+        Object error;
+        error.emplace_back(Pair("error", xbridge::xbridgeErrorText(xbridge::NO_SESSION)));
+        error.emplace_back(Pair("code", xbridge::NO_SESSION));
+        error.emplace_back(Pair("name", "dxCancelOrder"));
+        return error;
+    }
+
+    Object obj;
+    obj.emplace_back(Pair("id", id.GetHex()));
+
+    obj.emplace_back(Pair("maker", tx->fromCurrency));
+    obj.emplace_back(Pair("maker_size", xBridgeValueFromAmount(tx->fromAmount)));
+    obj.emplace_back(Pair("maker_address", connFrom->fromXAddr(tx->from)));
+
+    obj.emplace_back(Pair("taker", tx->toCurrency));
+    obj.emplace_back(Pair("taker_size", xBridgeValueFromAmount(tx->toAmount)));
+    obj.emplace_back(Pair("taker_address", connTo->fromXAddr(tx->to)));
+
+    obj.emplace_back(Pair("updated_at", bpt::to_iso_extended_string(tx->txtime)));
+    obj.emplace_back(Pair("created_at", bpt::to_iso_extended_string(tx->created)));
+
+    obj.emplace_back(Pair("status", tx->strState()));
+    return obj;
 }
 
 //******************************************************************************
