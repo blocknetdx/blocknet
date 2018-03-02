e29458bc9f5834098ca8119e243e0ad48c20101f
Michael M <mike@madgett.io>
2018-03-02 02:48:20 -0600

    Fix dxGetMyOrders

--- src/xbridge/rpcxbridge.cpp
+++ src/xbridge/rpcxbridge.cpp
@@ -1399,70 +1399,48 @@ json_spirit::Value dxGetMyOrders(const json_spirit::Array& params, bool fHelp)
     xbridge::App & xapp = xbridge::App::instance();
 
     Array r;
+    TransactionVector orders;
 
     TransactionMap trList = xbridge::App::instance().transactions();
 
-    if(trList.empty()) {
-
-        LOG() << "empty  transactions list ";
-        return r;
-
-    }
-
-    for(auto i : trList)
-    {
-
-        const auto& t = *i.second;
-
-        if(!t.isLocal())
+    // Filter local orders
+    for (auto i : trList) {
+        const xbridge::TransactionDescrPtr &t = i.second;
+        if(!t->isLocal())
             continue;
-
-        xbridge::WalletConnectorPtr connFrom = xapp.connectorByCurrency(t.fromCurrency);
-        xbridge::WalletConnectorPtr connTo   = xapp.connectorByCurrency(t.toCurrency);
-
-        Object o;
-
-        o.emplace_back(Pair("id", t.id.GetHex()));
-
-        // maker data
-        o.emplace_back(Pair("maker", t.fromCurrency));
-        o.emplace_back(Pair("maker_size", boost::lexical_cast<std::string>(util::xBridgeValueFromAmount(t.fromAmount))));
-        o.emplace_back(Pair("maker_address", connFrom->fromXAddr(t.from)));
-        // taker data
-        o.emplace_back(Pair("taker", t.toCurrency));
-        o.emplace_back(Pair("taker_size", boost::lexical_cast<std::string>(util::xBridgeValueFromAmount(t.toAmount))));
-        o.emplace_back(Pair("taker_address", connFrom->fromXAddr(t.to)));
-        // dates
-        o.emplace_back(Pair("updated_at", util::iso8601(t.txtime)));
-        o.emplace_back(Pair("created_at", util::iso8601(t.created)));
-
-        // should we make returning value correspond to the description or vice versa?
-        // Order status: created|open|pending|filled|canceled
-        o.emplace_back(Pair("status", t.strState()));
-
-        r.emplace_back(o);
+        orders.push_back(t);
     }
 
     // Add historical orders
     TransactionMap history = xbridge::App::instance().history();
-    TransactionVector result;
 
+    // Filter local orders only
     for (auto &item : history) {
         const xbridge::TransactionDescrPtr &ptr = item.second;
         if (ptr->isLocal() &&
                 (ptr->state == xbridge::TransactionDescr::trFinished ||
                  ptr->state == xbridge::TransactionDescr::trCancelled)) {
-            result.push_back(ptr);
+            orders.push_back(ptr);
         }
     }
 
-    std::sort(result.begin(), result.end(),
+    // Return if no records
+    if (orders.empty())
+        return r;
+
+    // sort ascending by updated time
+    std::sort(orders.begin(), orders.end(),
         [](const xbridge::TransactionDescrPtr &a,  const xbridge::TransactionDescrPtr &b) {
             return (a->txtime) > (b->txtime);
         });
 
-    Array arr;
-    for (const auto &t : result) {
+    std::map<std::string, bool> seen;
+    for (const auto &t : orders) {
+        // do not process already seen orders
+        if (seen.count(t->id.GetHex()))
+            continue;
+        seen[t->id.GetHex()] = true;
+
         xbridge::WalletConnectorPtr connFrom = xapp.connectorByCurrency(t->fromCurrency);
         xbridge::WalletConnectorPtr connTo   = xapp.connectorByCurrency(t->toCurrency);
 
