5866abe37129387b6ca1bc2c48f02425fbe78a24
Michael M <mike@madgett.io>
2018-03-12 14:18:23 -0500

    Check lower limit amounts

--- src/xbridge/rpcxbridge.cpp
+++ src/xbridge/rpcxbridge.cpp
@@ -553,6 +553,11 @@ Value dxMakeOrder(const Array &params, bool fHelp)
         return util::makeError(xbridge::INVALID_PARAMETERS, __FUNCTION__,
                                "Maximum supported size is " + std::to_string(xbridge::TransactionDescr::MAX_COIN));
     }
+    // Check lower limits
+    if (fromAmount <= 0 || toAmount <= 0) {
+        return util::makeError(xbridge::INVALID_PARAMETERS, __FUNCTION__,
+                               "Minimum supported size is " + util::xBridgeStringValueFromPrice(1.0/xbridge::TransactionDescr::COIN));
+    }
 
     auto statusCode = xbridge::SUCCESS;
 
