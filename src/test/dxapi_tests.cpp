c91a1e258c4fe223c80f2fff8ad5ed62e3f1ae5d
boa85 <belaev.oa85@gmail.com>
2018-03-20 01:45:38 +0600

    added template for unit tests

--- src/test/dxapi_tests.cpp
+++ src/test/dxapi_tests.cpp
@@ -9,9 +9,26 @@
 
 using namespace std;
 using namespace json_spirit;
+
+extern Value CallRPC(string args);
+
 BOOST_AUTO_TEST_SUITE(dxapi_tests)
-BOOST_AUTO_TEST_CASE(dxapi_test)
+
+
+BOOST_AUTO_TEST_CASE(dx_get_local_tokens)
 {
-    BOOST_CHECK_EQUAL(1,2);
+    BOOST_CHECK_THROW(CallRPC("dxGetLocalTokens"), runtime_error);
 }
+
+BOOST_AUTO_TEST_CASE(dx_get_network_tokens)
+{
+    BOOST_CHECK_THROW(CallRPC("dxGetNetworkTokens"), runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(dx_get_token_balances)
+{
+    BOOST_CHECK_THROW(CallRPC("dxGetTokenBalances"), runtime_error);
+}
+
+
 BOOST_AUTO_TEST_SUITE_END()
