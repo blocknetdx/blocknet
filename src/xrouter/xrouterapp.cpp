d7a0d5a82ae60073b864ee9d9a2414b7a12cbd57
Ryan Burn <ryan.burn@gmail.com>
2018-04-06 15:48:45 -0400

    Run clang-format.

--- src/xrouter/xrouterapp.cpp
+++ src/xrouter/xrouterapp.cpp
@@ -2,13 +2,13 @@
 //*****************************************************************************
 
 #include "xrouterapp.h"
-#include "util/xutil.h"
-#include "net.h"
-#include "wallet.h"
-#include "keystore.h"
 #include "init.h"
+#include "keystore.h"
 #include "main.h"
+#include "net.h"
 #include "script/standard.h"
+#include "util/xutil.h"
+#include "wallet.h"
 
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <iostream>
@@ -122,7 +122,7 @@ bool App::Impl::stop()
 
 //*****************************************************************************
 //*****************************************************************************
-void App::sendPacket(const XRouterPacketPtr & packet)
+void App::sendPacket(const XRouterPacketPtr& packet)
 {
     static std::vector<unsigned char> addr(20, 0);
     m_p->onSend(addr, packet->body());
@@ -162,99 +162,99 @@ void App::Impl::onSend(const std::vector<unsigned char>& id, const std::vector<u
 
 //*****************************************************************************
 //*****************************************************************************
-void App::sendPacket(const std::vector<unsigned char> & id, const XRouterPacketPtr & packet)
+void App::sendPacket(const std::vector<unsigned char>& id, const XRouterPacketPtr& packet)
 {
     m_p->onSend(id, packet->body());
 }
 
 //*****************************************************************************
 //*****************************************************************************
-static bool verifyBlockRequirement(const XRouterPacketPtr& packet) {
-  if (packet->size() < 36) {
-    std::clog << "packet not big enough\n";
-    return false;
-  }
-
-  uint256 txHash(packet->data());
-  int offset = 32;
-  uint32_t vout = *static_cast<uint32_t*>(static_cast<void*>(packet->data() + offset));
-
-  CCoins coins;
-  if (!pcoinsTip->GetCoins(txHash, coins)) {
-    std::clog << "Could not find " << txHash.ToString() << "\n";
-    return false;
-  }
-
-  if (vout > coins.vout.size()) {
-    std::clog << "Invalid vout index " << vout << "\n";
-    return false;
-  }
-
-  auto& txOut = coins.vout[vout];
-
-  if (txOut.nValue < minBlock) {
-    std::clog << "Insufficient BLOCK " << coins.vout[vout].nValue << "\n";
-    return false;
-  }
-
-  CTxDestination destination;
-  if(!ExtractDestination(txOut.scriptPubKey, destination)) {
-    std::clog << "Unable to extract destination\n";
-    return false;
-  }
-
-  auto txKeyID = boost::get<CKeyID>(&destination);
-  if (!txKeyID) {
-    std::clog << "destination must be a single address\n";
-    return false;
-  }
-
-  CPubKey packetKey(packet->pubkey(),
-      packet->pubkey() + XRouterPacket::pubkeySize);
-
-  if (packetKey.GetID() != *txKeyID) {
-    std::clog << "Public key provided doesn't match UTXO destination.\n";
-    return false;
-  }
-
-  std::cout << "destination = " << txKeyID->ToString() << "\n";
-  std::cout << "packet keyid = " << packetKey.GetID().ToString() << "\n";
-  std::cout << "distnation.which() = " << destination.which() << "\n";
-  std::cout << "txHash = " << txHash.ToString() << "\n";
-  std::cout << "vout = " << vout << "\n";
-  std::cout << "value = " << coins.vout[vout].nValue << "\n";
-  return true;
-}
-
-//*****************************************************************************
-//*****************************************************************************
-static bool processGetBlocks(XRouterPacketPtr packet) {
-  std::cout << "Processing GetBlocks\n";
-  if (!verifyBlockRequirement(packet)) {
-    std::clog << "Block requirement not satisfied\n";
-    return false;
-  }
-  return true;
-}
-
-//*****************************************************************************
-//*****************************************************************************
-void App::onMessageReceived(const std::vector<unsigned char> & id,
-                            const std::vector<unsigned char> & message,
-                            CValidationState & /*state*/)
+static bool verifyBlockRequirement(const XRouterPacketPtr& packet)
 {
-        std::cerr << "Received xrouter packet\n";
+    if (packet->size() < 36) {
+        std::clog << "packet not big enough\n";
+        return false;
+    }
+
+    uint256 txHash(packet->data());
+    int offset = 32;
+    uint32_t vout = *static_cast<uint32_t*>(static_cast<void*>(packet->data() + offset));
+
+    CCoins coins;
+    if (!pcoinsTip->GetCoins(txHash, coins)) {
+        std::clog << "Could not find " << txHash.ToString() << "\n";
+        return false;
+    }
+
+    if (vout > coins.vout.size()) {
+        std::clog << "Invalid vout index " << vout << "\n";
+        return false;
+    }
+
+    auto& txOut = coins.vout[vout];
+
+    if (txOut.nValue < minBlock) {
+        std::clog << "Insufficient BLOCK " << coins.vout[vout].nValue << "\n";
+        return false;
+    }
+
+    CTxDestination destination;
+    if (!ExtractDestination(txOut.scriptPubKey, destination)) {
+        std::clog << "Unable to extract destination\n";
+        return false;
+    }
+
+    auto txKeyID = boost::get<CKeyID>(&destination);
+    if (!txKeyID) {
+        std::clog << "destination must be a single address\n";
+        return false;
+    }
+
+    CPubKey packetKey(packet->pubkey(),
+        packet->pubkey() + XRouterPacket::pubkeySize);
+
+    if (packetKey.GetID() != *txKeyID) {
+        std::clog << "Public key provided doesn't match UTXO destination.\n";
+        return false;
+    }
+
+    std::cout << "destination = " << txKeyID->ToString() << "\n";
+    std::cout << "packet keyid = " << packetKey.GetID().ToString() << "\n";
+    std::cout << "distnation.which() = " << destination.which() << "\n";
+    std::cout << "txHash = " << txHash.ToString() << "\n";
+    std::cout << "vout = " << vout << "\n";
+    std::cout << "value = " << coins.vout[vout].nValue << "\n";
+    return true;
+}
+
+//*****************************************************************************
+//*****************************************************************************
+static bool processGetBlocks(XRouterPacketPtr packet)
+{
+    std::cout << "Processing GetBlocks\n";
+    if (!verifyBlockRequirement(packet)) {
+        std::clog << "Block requirement not satisfied\n";
+        return false;
+    }
+    return true;
+}
+
+//*****************************************************************************
+//*****************************************************************************
+void App::onMessageReceived(const std::vector<unsigned char>& id,
+    const std::vector<unsigned char>& message,
+    CValidationState& /*state*/)
+{
+    std::cerr << "Received xrouter packet\n";
 
     XRouterPacketPtr packet(new XRouterPacket);
-    if (!packet->copyFrom(message))
-    {
-      std::clog << "incorrect packet received " << __FUNCTION__;
+    if (!packet->copyFrom(message)) {
+        std::clog << "incorrect packet received " << __FUNCTION__;
         return;
     }
 
-    if (!packet->verify())
-    {
-      std::clog << "unsigned packet or signature error " << __FUNCTION__;
+    if (!packet->verify()) {
+        std::clog << "unsigned packet or signature error " << __FUNCTION__;
         return;
     }
 
@@ -262,10 +262,10 @@ void App::onMessageReceived(const std::vector<unsigned char> & id,
     /*           << " command " << packet->command(); */
 
     switch (packet->command()) {
-      case xrGetBlocks:
+    case xrGetBlocks:
         processGetBlocks(packet);
         break;
-      default:
+    default:
         std::clog << "Unkown packet\n";
         break;
     }
@@ -283,20 +283,20 @@ static bool satisfyBlockRequirement(
     }
     for (auto& addressCoins : pwalletMain->AvailableCoinsByAddress()) {
         for (auto& output : addressCoins.second) {
-          if (output.Value() >= minBlock) {
-            CKeyID keyID;
-            if (!addressCoins.first.GetKeyID(keyID)) {
-              std::cerr << "GetKeyID failed\n";
-              continue;
+            if (output.Value() >= minBlock) {
+                CKeyID keyID;
+                if (!addressCoins.first.GetKeyID(keyID)) {
+                    std::cerr << "GetKeyID failed\n";
+                    continue;
+                }
+                if (!pwalletMain->GetKey(keyID, key)) {
+                    std::cerr << "GetKey failed\n";
+                    continue;
+                }
+                txHash = output.tx->GetHash();
+                vout = output.i;
+                return true;
             }
-            if (!pwalletMain->GetKey(keyID, key)) {
-              std::cerr << "GetKey failed\n";
-              continue;
-            }
-            txHash = output.tx->GetHash();
-            vout = output.i;
-            return true;
-          }
         }
     }
     return false;
@@ -312,8 +312,8 @@ Error App::getBlocks(uint256& id)
     uint32_t vout;
     CKey key;
     if (!satisfyBlockRequirement(txHash, vout, key)) {
-      std::cerr << "Minimum block requirement not satisfied\n";
-      return UNKNOWN_ERROR;
+        std::cerr << "Minimum block requirement not satisfied\n";
+        return UNKNOWN_ERROR;
     }
     std::cout << "txHash = " << txHash.ToString() << "\n";
     std::cout << "vout = " << vout << "\n";
