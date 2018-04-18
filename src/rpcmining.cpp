0cb77cc88882e4cabc113f0f7871b05f0ae88862
wqking <wqking@outlook.com>
2018-04-18 12:08:48 +0800

    In regtest, changed getblocktemplate to not to check peer existence to ease the test.

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -427,10 +427,10 @@ UniValue getblocktemplate(const UniValue& params, bool fHelp)
     if (strMode != "template")
         throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid mode");
 
-    if (vNodes.empty())
+    if (vNodes.empty() && Params().NetworkID() != CBaseChainParams::REGTEST)
         throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "Phore is not connected!");
 
-    if (IsInitialBlockDownload())
+    if (IsInitialBlockDownload() && Params().NetworkID() != CBaseChainParams::REGTEST)
         throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "Phore is downloading blocks...");
 
     static unsigned int nTransactionsUpdatedLast;
