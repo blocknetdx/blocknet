3e816eb2d4e39cd7b3e2b9556df1dd915d2b6f89
Daniel A. Zorin <jfaranime@gmail.com>
2018-05-29 09:25:25 -0400

    Fix windows uuid gen
    Signed-off-by: Daniel A. Zorin <jfaranime@gmail.com>

--- src/xrouter/xrouterapp.cpp
+++ src/xrouter/xrouterapp.cpp
@@ -51,10 +51,10 @@ static std::string generateUUID()
     char guid_string[37];
     sprintf(guid_string, sizeof(guid_string) / sizeof(guid_string[0]),
           "%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x",
-          guid->Data1, guid->Data2, guid->Data3,
-          guid->Data4[0], guid->Data4[1], guid->Data4[2],
-          guid->Data4[3], guid->Data4[4], guid->Data4[5],
-          guid->Data4[6], guid->Data4[7]);
+          guid.Data1, guid.Data2, guid.Data3,
+          guid.Data4[0], guid.Data4[1], guid.Data4[2],
+          guid.Data4[3], guid.Data4[4], guid.Data4[5],
+          guid.Data4[6], guid.Data4[7]);
     return guid_string;
 }
     
