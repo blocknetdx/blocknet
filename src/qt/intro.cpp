72bbc5d6de7b8f67b45cbe704904e849aee5fdc3
Michael M <mike@madgett.io>
2018-05-30 20:09:19 -0500

    Renamed BlocknetDX Core to Blocknet

--- src/qt/intro.cpp
+++ src/qt/intro.cpp
@@ -175,7 +175,7 @@ bool Intro::pickDataDirectory()
                 TryCreateDirectory(GUIUtil::qstringToBoostPath(dataDir));
                 break;
             } catch (fs::filesystem_error& e) {
-                QMessageBox::critical(0, tr("BlocknetDX Core"),
+                QMessageBox::critical(0, tr("Blocknet"),
                     tr("Error: Specified data directory \"%1\" cannot be created.").arg(dataDir));
                 /* fall through, back to choosing screen */
             }
