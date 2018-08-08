123b59fd29fb9c7ccaabfa517aeade24351514b2
Joe <joe@phore.io>
2018-08-07 21:48:27 -0700

    Initial Round of UI Overhaul
    Dark Theme matching Phore Branding throughout including custom icons, custom fonts, matching button styles, etc.

--- src/qt/transactionview.cpp
+++ src/qt/transactionview.cpp
@@ -133,6 +133,7 @@ TransactionView::TransactionView(QWidget* parent) : QWidget(parent), model(0), t
     vlayout->setSpacing(0);
 
     QTableView* view = new QTableView(this);
+    view->setShowGrid(false);
     vlayout->addLayout(hlayout);
     vlayout->addWidget(createDateRangeWidget());
     vlayout->addWidget(view);
