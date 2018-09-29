93822db3f86a3e1a8c5c1518354bd9825387b9c5
Julian Meyer <julianmeyer2000@gmail.com>
2018-09-29 15:02:32 -0700

    Update testnet params

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -253,10 +253,10 @@ public:
         nMasternodeCountDrift = 4;
         nModifierUpdateBlock = 51197; //approx Mon, 17 Apr 2017 04:00:00 GMT
         nMaxMoneyOut = 43199500 * COIN;
-        nLastPOWBlock = 50000;
-        nZerocoinStartHeight = 200;
+        nLastPOWBlock = 5000;
+        nZerocoinStartHeight = 10000;
 
-        nZerocoinLastOldParams = 100000000;
+        nZerocoinLastOldParams = 50000;
 
         //! Modify the testnet genesis block so the timestamp is valid for a later start.
         genesis.nTime = 1505224800;
