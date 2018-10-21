9af982dfcce752edad5aef37fdc344b27c7566e0
tohsnoom <moonshot@phore.io>
2018-10-20 23:02:55 -0500

    Reverted alternate protocol enforcement spork

--- src/main.cpp
+++ src/main.cpp
@@ -6624,7 +6624,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 //       it was the one which was commented out
 int ActiveProtocol()
 {
-    if (IsSporkActive(SPORK_15_NEW_PROTOCOL_ENFORCEMENT_2))
+    if (IsSporkActive(SPORK_14_NEW_PROTOCOL_ENFORCEMENT))
         return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
     return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT;
 }
