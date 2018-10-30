39c0e755f137b8397a2abc18f90b76e82655fa74
Joe <joe@phore.io>
2018-10-30 11:25:16 -0700

    Match Splash Status text color with Statusbar Status text color

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -51,7 +51,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle* networkStyle) 
     QPainter pixPaint(&pixmap);
     pixPaint.setRenderHint(QPainter::Antialiasing);
     pixPaint.setRenderHint(QPainter::HighQualityAntialiasing);
-    pixPaint.setPen(QColor(100, 100, 100));
+    pixPaint.setPen(QColor(73, 84, 94));
 
     // check font size and drawing with
     pixPaint.setFont(QFont(font, 28 * fontFactor));
@@ -119,7 +119,7 @@ static void InitMessage(SplashScreen* splash, const std::string& message)
         Qt::QueuedConnection,
         Q_ARG(QString, QString::fromStdString(message)),
         Q_ARG(int, Qt::AlignBottom | Qt::AlignHCenter),
-        Q_ARG(QColor, QColor(100, 100, 100)));
+        Q_ARG(QColor, QColor(73, 84, 94)));
 }
 
 static void ShowProgress(SplashScreen* splash, const std::string& title, int nProgress)
