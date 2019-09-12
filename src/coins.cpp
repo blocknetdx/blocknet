d294af0fa5eb6c3de9f7301971739af507da46d9
M <magic.ru789@gmail.com>
2019-09-12 18:28:09 -0500

    Blocknet PoS adjust coinbase overwrite flag in coinsdb

--- src/coins.cpp
+++ src/coins.cpp
@@ -89,7 +89,7 @@ void AddCoins(CCoinsViewCache& cache, const CTransaction &tx, int nHeight, bool 
     bool fCoinbase = tx.IsCoinBase();
     const uint256& txid = tx.GetHash();
     for (size_t i = 0; i < tx.vout.size(); ++i) {
-        bool overwrite = check ? cache.HaveCoin(COutPoint(txid, i)) : fCoinbase;
+        bool overwrite = check ? cache.HaveCoin(COutPoint(txid, i)) : false; // Blocknet doesn't have pre-BIP30 duplicate coinbase txs
         // Always set the possible_overwrite flag to AddCoin for coinbase txn, in order to correctly
         // deal with the pre-BIP30 occurrences of duplicate coinbase transactions.
         cache.AddCoin(COutPoint(txid, i), Coin(tx.vout[i], nHeight, fCoinbase || tx.IsCoinStake()), overwrite); // coinsdb treats coinstake as coinbase
