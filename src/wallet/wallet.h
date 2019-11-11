ae4469d3d1a3479bbbbf3b0a4947666538f094de
M <magic.ru789@gmail.com>
2019-11-11 15:52:14 -0600

    Blocknet default tx confirm set to 1

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -87,7 +87,7 @@ static const bool DEFAULT_WALLET_REJECT_LONG_CHAINS = false;
 //! Default for -avoidpartialspends
 static const bool DEFAULT_AVOIDPARTIALSPENDS = false;
 //! -txconfirmtarget default
-static const unsigned int DEFAULT_TX_CONFIRM_TARGET = 6;
+static const unsigned int DEFAULT_TX_CONFIRM_TARGET = 1;
 //! -walletrbf default
 static const bool DEFAULT_WALLET_RBF = false;
 static const bool DEFAULT_WALLETBROADCAST = true;
