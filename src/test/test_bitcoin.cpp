ec60bbf20d05f044018ac6157be320a06fdf0216
M <magic.ru789@gmail.com>
2019-12-30 12:54:34 -0600

    Blocknet ensure ecc stop before start

--- src/test/test_bitcoin.cpp
+++ src/test/test_bitcoin.cpp
@@ -35,6 +35,7 @@ BasicTestingSetup::BasicTestingSetup(const std::string& chainName)
     : m_path_root(fs::temp_directory_path() / "test_blocknet" / strprintf("%lu_%i", (unsigned long)GetTime(), (int)(InsecureRandRange(1 << 30))))
 {
     SHA256AutoDetect();
+    ECC_Stop();
     ECC_Start();
     SetupEnvironment();
     SetupNetworking();
