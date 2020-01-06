933a60e205474cd8b42c7e11a5e9c5c078e4744d
M <magic.ru789@gmail.com>
2020-01-06 13:42:22 -0600

    Blocknet set default checklevel on block validation to 3

--- src/validation.h
+++ src/validation.h
@@ -202,7 +202,7 @@ static const unsigned int MIN_BLOCKS_TO_KEEP = 288;
 static const unsigned int NODE_NETWORK_LIMITED_MIN_BLOCKS = 288;
 
 static const signed int DEFAULT_CHECKBLOCKS = 100;
-static const unsigned int DEFAULT_CHECKLEVEL = 4;
+static const unsigned int DEFAULT_CHECKLEVEL = 3;
 
 // Require that user allocate at least 550 MiB for block & undo files (blk???.dat and rev???.dat)
 // At 1MB per block, 288 blocks = 288MB.
