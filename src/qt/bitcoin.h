cc0fc5db7ed5fb901bb7dcdb70702f16cd58a016
M <magic.ru789@gmail.com>
2020-01-30 17:22:45 -0600

    Blocknet update copyright dates

--- src/qt/bitcoin.h
+++ src/qt/bitcoin.h
@@ -1,4 +1,5 @@
 // Copyright (c) 2011-2016 The Bitcoin Core developers
+// Copyright (c) 2019-2020 The Blocknet developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
