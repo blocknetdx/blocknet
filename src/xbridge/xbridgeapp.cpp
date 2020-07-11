1362c349a4675516e250aeeacd71ac7c3307c80f
M <magic.ru789@gmail.com>
2020-07-11 17:42:49 -0500

    [xbridge] support BCD

--- src/xbridge/xbridgeapp.cpp
+++ src/xbridge/xbridgeapp.cpp
@@ -18,6 +18,7 @@
 #include <xbridge/xbridgesession.h>
 #include <xbridge/xbridgewalletconnector.h>
 #include <xbridge/xbridgewalletconnectorbtc.h>
+#include <xbridge/xbridgewalletconnectorbcd.h>
 #include <xbridge/xbridgewalletconnectorbch.h>
 #include <xbridge/xbridgewalletconnectordgb.h>
 #include <xbridge/xbridgepacket.h>
@@ -1042,6 +1043,11 @@ void App::updateActiveWallets()
         else if (wp.method == "BTC" || wp.method == "SYS")
         {
             conn.reset(new BtcWalletConnector<BtcCryptoProvider>);
+            *conn = wp;
+        }
+        else if (wp.method == "BCD")
+        {
+            conn.reset(new BCDWalletConnector);
             *conn = wp;
         }
         else if (wp.method == "BCH")
---
+++ src/xbridge/xbridgewalletconnectorbcd.h
@@ -0,0 +1,49 @@
+// Copyright (c) 2017-2020 The Blocknet developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+//******************************************************************************
+//******************************************************************************
+
+#ifndef BLOCKNET_XBRIDGE_XBRIDGEWALLETCONNECTORBCD_H
+#define BLOCKNET_XBRIDGE_XBRIDGEWALLETCONNECTORBCD_H
+
+#include <xbridge/xbridgewalletconnectorbtc.h>
+#include <xbridge/xbridgecryptoproviderbtc.h>
+
+//*****************************************************************************
+//*****************************************************************************
+namespace xbridge
+{
+
+//******************************************************************************
+//******************************************************************************
+class BCDWalletConnector : public BtcWalletConnector<BtcCryptoProvider>
+{
+public:
+    BCDWalletConnector();
+
+public:
+    bool createRefundTransaction(const std::vector<XTxIn> & inputs,
+                                 const std::vector<std::pair<std::string, double> > & outputs,
+                                 const std::vector<unsigned char> & mpubKey,
+                                 const std::vector<unsigned char> & mprivKey,
+                                 const std::vector<unsigned char> & innerScript,
+                                 const uint32_t lockTime,
+                                 std::string & txId,
+                                 std::string & rawTx) override;
+
+    bool createPaymentTransaction(const std::vector<XTxIn> & inputs,
+                                  const std::vector<std::pair<std::string, double> > & outputs,
+                                  const std::vector<unsigned char> & mpubKey,
+                                  const std::vector<unsigned char> & mprivKey,
+                                  const std::vector<unsigned char> & xpubKey,
+                                  const std::vector<unsigned char> & innerScript,
+                                  std::string & txId,
+                                  std::string & rawTx) override;
+
+};
+
+} // namespace xbridge
+
+#endif // BLOCKNET_XBRIDGE_XBRIDGEWALLETCONNECTORBCD_H
