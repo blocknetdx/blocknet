1c33326cb51aac29277bae18326e689ae27f1561
M <magic.ru789@gmail.com>
2020-09-22 11:49:14 -0500

    [gui] Ref upstream change: 2800b3d5c14a9e5437b7e8838c69fdd6710908bb

--- src/qt/blocknetgui.cpp
+++ src/qt/blocknetgui.cpp
@@ -337,6 +337,7 @@ void BlocknetGUI::createActions()
     m_open_wallet_action->setEnabled(false);
     m_open_wallet_action->setMenu(new QMenu(this));
     m_open_wallet_action->setStatusTip(tr("Open a wallet"));
+    m_open_wallet_menu = new QMenu(this);
 
     m_close_wallet_action = new QAction(tr("Close Wallet..."), this);
     m_close_wallet_action->setStatusTip(tr("Close wallet"));
@@ -386,13 +387,14 @@ void BlocknetGUI::createActions()
         connect(usedSendingAddressesAction, &QAction::triggered, wf, &BlocknetWallet::usedSendingAddresses);
         connect(usedReceivingAddressesAction, &QAction::triggered, wf, &BlocknetWallet::usedReceivingAddresses);
         connect(openAction, &QAction::triggered, this, &BlocknetGUI::openClicked);
-        connect(m_open_wallet_action->menu(), &QMenu::aboutToShow, [this] {
+        connect(m_open_wallet_menu, &QMenu::aboutToShow, [this] {
+            m_open_wallet_menu->clear();
             m_open_wallet_action->menu()->clear();
             std::vector<std::string> available_wallets = m_wallet_controller->getWalletsAvailableToOpen();
             std::vector<std::string> wallets = m_node.listWalletDir();
             for (const auto& path : wallets) {
                 QString name = path.empty() ? QString("["+tr("default wallet")+"]") : QString::fromStdString(path);
-                QAction* action = m_open_wallet_action->menu()->addAction(name);
+                QAction* action = m_open_wallet_menu->addAction(name);
 
                 if (std::find(available_wallets.begin(), available_wallets.end(), path) == available_wallets.end()) {
                     // This wallet is already loaded
@@ -428,7 +430,7 @@ void BlocknetGUI::createActions()
                 });
             }
             if (wallets.empty()) {
-                QAction* action = m_open_wallet_action->menu()->addAction(tr("No wallets available"));
+                QAction* action = m_open_wallet_menu->addAction(tr("No wallets available"));
                 action->setEnabled(false);
             }
         });
@@ -662,7 +664,7 @@ void BlocknetGUI::setWalletController(WalletController* wallet_controller)
     m_wallet_controller = wallet_controller;
 
     m_open_wallet_action->setEnabled(true);
-    m_open_wallet_action->setMenu(new QMenu(this));
+    m_open_wallet_action->setMenu(m_open_wallet_menu);
 
     connect(wallet_controller, &WalletController::walletAdded, this, &BlocknetGUI::addWallet);
     connect(wallet_controller, &WalletController::walletRemoved, this, &BlocknetGUI::removeWallet);
