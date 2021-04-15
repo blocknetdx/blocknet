eef18dd315b2cd2d5021b0f0ef3057b778098b75
corvaxx <maybe.mail@inbox.ru>
2021-04-15 23:42:14 +0300

    update checkpoints for new testnet
    Signed-off-by: corvaxx <maybe.mail@inbox.ru>

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -329,8 +329,7 @@ public:
         checkpointData = {
             {
                 {0, uint256S("0x0fd62ae4f74c7ee0c11ef60fc5a2e69a5c02eaee2e77b21c3db70934b5a5c8b9")},
-                {50, uint256S("0x00031ab84aadb151e91a0ed96761e9e8174485f0a4631fd7e86e9eafb75e49a6")},
-                {1040, uint256S("0x48fe73d7ef667bf3c06179c86478e0c0ac18efec175fadc16f6a382f476185be")},
+                {50, uint256S("0x0003f993248beab1bb9ad8d470e858e320fb9670e1f1a93058779bc4a2553e77")},
             }
         };
         // Assign last checkpoint height, used to estimate initial load time
